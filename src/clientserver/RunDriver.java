3696fbc25cd0f4e3923ae04005180d45d4014566
Evan Friday <evan_friday@hotmail.com>
2014-03-05 23:33:11 -0800

    mode added

--- src/clientserver/RunDriver.java
+++ src/clientserver/RunDriver.java
@@ -8,18 +8,36 @@ public class RunDriver {
 	
 	final static int ACCEPTING_DATA = 0;
 	final static int PROPAGATING_DATA = 1;
-	
+	final static int RECEIVE_ONLY = 0;
+	final static int GOSSIP = 1;
 	
 	public static int STATUS = ACCEPTING_DATA;
+	public static int MODE;
 	
-	
-	public static void main(String[] args) throws OutOfMemoryError, IOException {
+	public static int main(String[] args) throws OutOfMemoryError, IOException {
+		MODE = (Integer) Integer.parseInt(args[0]);
+		if(MODE != RECEIVE_ONLY || MODE != GOSSIP){
+			System.err.print("You have specified an invalid mode");
+			return 1;
+		}
+		
 		while(true){
 			switch(STATUS){
 			case ACCEPTING_DATA:
 				//read in new data
 				Server.acceptUpdate();
-				STATUS=PROPAGATING_DATA;
+				
+				//Depending on mode specified, either continuously accept data, or propagate as well
+				switch(MODE){
+				case RECEIVE_ONLY: 	
+					STATUS=PROPAGATING_DATA;
+					break;
+				case GOSSIP:
+					STATUS=ACCEPTING_DATA;
+					break;
+				default: 			
+				}
+				
 				break;
 			case PROPAGATING_DATA:
 				//Connect to other nodes, and send data.
