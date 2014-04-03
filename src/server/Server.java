c206b0f67ca917f4c666d493d0b62710c8a7fe17
Cameron Johnston <cameron.johnston8@gmail.com>
2014-04-02 23:34:23 -0700

    Take 2?

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,10 +1,14 @@
 package server;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.*;
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.concurrent.ConcurrentHashMap;
 
+import message.Key;
+import message.Value;
 import node.NodeData;
 import sun.org.mozilla.javascript.internal.Node;
 
@@ -12,9 +16,9 @@ public class Server {
 	Node nodeList;
 	ServerSocket server;
 	ArrayList all_nodes = new ArrayList<NodeData>();
-	LinkedList primary_space = new LinkedList<Node>();
-	LinkedList child1_space = new LinkedList<Node>();
-	LinkedList child2_space = new LinkedList<Node>();
+	ConcurrentHashMap primary_space = new ConcurrentHashMap<Key, Value>();
+	ConcurrentHashMap replica1_space = new ConcurrentHashMap<Key, Value>();
+	ConcurrentHashMap replica2_space = new ConcurrentHashMap<Key, Value>();
 	
 	//CONSTRUCTOR
 	public Server() throws IOException {
@@ -37,6 +41,20 @@ public class Server {
 		 */
 	}
 	
-	
+	public void fileRead(String file_location) throws Exception{
+		FileReader file = new FileReader(file_location);
+		BufferedReader in = new BufferedReader(file);
+		String line = in.readLine();
+		NodeData nd;
+		while(line != null) {
+			InetAddress addr = InetAddress.getByName(line);
+			if(addr.isReachable(5000)) // Ping with 5-second timeout
+				nd = new NodeData(line, true); // Alive node
+			else
+				nd = new NodeData(line, false); // Dead node
+			all_nodes.add(nd);
+		}
+		in.close();
+	}
 
 }
