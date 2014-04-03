6ee2aa0fd649586814503c3f4d23c952c7ebeb4d
Cameron Johnston <cameron.johnston8@gmail.com>
2014-04-02 22:34:14 -0700

    Added a bit more to Server.java

--- src/server/Server.java
+++ src/server/Server.java
@@ -2,12 +2,19 @@ package server;
 
 import java.io.IOException;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
+import node.NodeData;
 import sun.org.mozilla.javascript.internal.Node;
 
 public class Server {
 	Node nodeList;
 	ServerSocket server;
+	ArrayList all_nodes = new ArrayList<NodeData>();
+	LinkedList primary_space = new LinkedList<Node>();
+	LinkedList child1_space = new LinkedList<Node>();
+	LinkedList child2_space = new LinkedList<Node>();
 	
 	//CONSTRUCTOR
 	public Server() throws IOException {
