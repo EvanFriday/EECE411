3e72fe3049cd5a3887a27924ac9e73931aa7ae54
Evan Friday <evan_friday@hotmail.com>
2014-04-03 00:08:54 -0700

    fixed .gitignore

---
+++ src/tools/Value.java
@@ -0,0 +1,38 @@
+package tools;
+
+public class Value {
+	public static final int SIZE = 1024;
+
+	private byte[] value;
+
+	public Value() {
+		this.value = new byte[SIZE];
+	}
+
+	public Value(byte[] value) {
+		this();
+		for(int i = 0; i< Value.SIZE; i++){
+			this.value[i]=value[i];
+		}
+	}
+	public Value(Value v){
+		this();
+		for(int i = 0; i< Value.SIZE; i++){
+			this.value[i]=v.getValue(i);
+		}
+	}
+
+	public Value(byte[] message, int offset) {
+		this();
+		for (int i = 0; i < SIZE; i++) {
+			this.setValue(message[offset + i], i);
+		}
+	}
+
+	public byte getValue(int index) {
+		return this.value[index];
+	}
+	public void setValue(byte value, int index){
+		this.value[index] = value;
+	}
+}
