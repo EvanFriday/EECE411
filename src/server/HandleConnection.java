7a2f317ff1f75fa29d2706e8e808ac924bf9364c
Evan Friday <evan_friday@hotmail.com>
2014-04-20 18:03:41 -0700

    Deleted Old Commented out code!

--- src/server/HandleConnection.java
+++ src/server/HandleConnection.java
@@ -56,27 +56,15 @@ public class HandleConnection implements Runnable {
 				Tools.print("Failed to open input or output stream");
 			}
 			try {
-//				in.read(message);
 				message.getFrom(in);
 			} catch (IOException e) {
 				Tools.print("failed to read message");
 			}
-//			message = new Message(message);
-			Command c = null; 
+			
+			Command c = (Command) message.getLeadByte(); 
 			Key k = new Key(message.getMessageKey());
 			Value v = new Value(message.getMessageValue());
-//			byte[] replyv = new byte[1024];
 			EVpair pair = new EVpair(null,null);
-//			for(int i=0;i<message.length;i++){
-//				if(i==0)
-//					c = Command.getCommand(message[i]);
-//				else if(1<=i && i<33)
-//					k.setValue(message[i], i-1);
-//				else
-//					v.setValue(message[i], i-1-32);
-//			}
-			
-			c = (Command) message.getLeadByte();
 			List<Node> propagate_to_list = new ArrayList<Node>();
 			Map<String,Message> replies = new ConcurrentHashMap<String,Message>();
 			//correct_node_for_key = getCorrectNode(k); //USE THIS FOR NOMAL USE
@@ -95,18 +83,13 @@ public class HandleConnection implements Runnable {
 				switch(c) {
 				case PUT:
 					Tools.print("PUT");
-//					replyerr = this.server.getNode().addToKvpairs(k, v);
 					local_reply.setLeadByte(this.server.getNode().addToKvpairs(k, v));
 					break;
 				case GET:
 					Tools.print("GET");
 					pair = server.getNode().getValueFromKvpairs(k);
-//					replyerr = pair.getError();
 					local_reply.setLeadByte(pair.getError());
-//					if(pair.getValue()!= null){
-//						replyv = pair.getValue().value;
-						local_reply.setMessageValue(pair.getValue());
-//					}
+					local_reply.setMessageValue(pair.getValue());
 					break;
 				case REMOVE:
 					Tools.print("RM");
@@ -120,16 +103,7 @@ public class HandleConnection implements Runnable {
 			Tools.printByte(k.key);
 			if(v != null)
 				Tools.printByte(v.value);
-//			for(int i=0;i<reply.length;i++){
-//				if(i==0)
-//					reply[i] = replyerr.getByte();
-//				else if(replyv!=null)
-//					reply[i] = replyv[i-1];
-//			}
-//			Tools.print("message value = ");
-			//Tools.printByte(local_reply.getMessageValue().value);
 			try {
-				//out.write(reply);
 				local_reply.sendReplyTo(out);
 			} catch (Exception e) {
 				Tools.print("failed to write reply");
